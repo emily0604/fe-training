b9ea87d641f8d83025d742d835e3d62e49a39c58
Ha <pein1625@gmail.com>
2017-11-14 16:09:13 +0700

    Lesson 1

---
+++ lesson1/dev/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  $('.menu-button').click(function(){
+    $(this).siblings('ul').slideToggle();
+  });
+
+  function showNext(next){
+    if (next > 2) next = 0;
+    if (next < 0) next = 2;
+    var left = -next * 100 + "%";
+    $('.slider').css('left', left);
+    $('#banner li.active').removeClass('active');
+    $('#banner li[data-slider="'+next+'"').addClass('active');
+    current = next;
+  }
+
+  function setSlider(){
+    interval = setInterval(function(){
+      showNext(current+1);
+    }, 2000);
+  }
+
+  function clearSlider(){
+    clearInterval(interval);
+  }
+
+  var interval;
+  var current = 0;
+
+  setSlider();
+  $('#banner').mouseover(clearSlider).mouseout(setSlider);
+
+  $('#banner li').click(function(){
+    var next = $(this).data('slider');
+    showNext(next);
+  });
+
+  function slideNext(next){
+    if (next > 2) next = 0;
+    if (next < 0) next = 2;
+    var left = -next * 100 + "%";
+    $('.slide-wrapper').css('left', left);
+    $('.client li.active').removeClass('active');
+    $('.client li[data-slider="'+next+'"').addClass('active');
+    current1 = next;        
+  }
+
+  function setSlider1(){
+    interval1 = setInterval(function(){
+      slideNext(current1+1);
+    }, 3000);
+  }
+
+  function clearSlider1(){
+    clearInterval(interval1);
+  }
+
+  var current1 = 0;
+  var interval1;
+  setSlider1();
+  $(".client li").click(function(){
+    var next = $(this).data('slider');
+    slideNext(next);
+  });
+});
